1ead4ddd0cc5e0fbd55915316bd3c563543bf0fd
sammo <samrhysperry93@hotmail.co.uk>
2021-02-18 16:54:11 +0000

    Refactor Day 3

--- lib/3/slope.rb
+++ lib/3/slope.rb
@@ -1,33 +1,41 @@
-def traverse_slope(drop, skip=false)
-  file = File.open("./data.txt", 'r').readlines
-  slope = []
-  file.each do |line|
-    slope << line.chomp * 120
+class Slope
+  attr_reader :file
+  def initialize
+    file = File.open("./lib/3/data.txt", 'r').readlines
   end
 
-  trees_hit = 0
-  position = 0
-  slope.each_with_index do |row, index|
-    if skip && index.odd?
-      next
+  def part_one
+    traverse_slope(3)
+  end
+
+  def part_two
+    a = traverse_slope(1)
+    b = traverse_slope(3)
+    c = traverse_slope(5)
+    d = traverse_slope(7)
+    e = traverse_slope(1, true)
+    f = a*b*c*d*e
+    p f
+  end
+
+  def traverse_slope(drop, skip=false)
+    slope = []
+    file.each do |line|
+      slope << line.chomp * 120
     end
 
-    puts row, position, row[position], index, "\n"
-    if row[position] == "#"
-      trees_hit += 1
+    trees_hit = 0
+    position = 0
+    slope.each_with_index do |row, index|
+      if skip && index.odd?
+        next
+      end
+
+      if row[position] == "#"
+        trees_hit += 1
+      end
+      position += drop
     end
-    position += drop
-    # if position > row.size
-    #   position -= 28
-    # end
+    trees_hit
   end
-  trees_hit
 end
-
-a = traverse_slope(1)
-b = traverse_slope(3)
-c = traverse_slope(5)
-d = traverse_slope(7)
-e = traverse_slope(1, true)
-f = a*b*c*d*e
-p f
