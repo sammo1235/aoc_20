a3ef778ffd8cff5eab8dad329e354a0a389a9d89
sammo <samrhysperry93@hotmail.co.uk>
2021-02-18 12:18:58 +0000

    Refactor day 2

--- lib/2/password_philosophy.rb
+++ lib/2/password_philosophy.rb
@@ -5,36 +5,34 @@ class Password
   end
 
   def part_one
-    count = 0
-
-    file.each do |line|
-      rule = line.split(':').first
-      letter = rule.split(" ").last
-      low_freq = rule.split(" ").first.split('-').first.to_i
-      high_freq = rule.split(" ").first.split('-').last.to_i
-      password = line.split(': ').last.chomp.split('')
-
-      letter_count = password.count(letter)
-
-      count += 1 if letter_count >= low_freq && letter_count <= high_freq
-    end
-    count
+    check_passwords(true)
   end
 
   def part_two
+    check_passwords(false)
+  end
+  
+  def check_passwords(p_one)
     count = 0
+
     file.each do |line|
       rule = line.split(':').first
       letter = rule.split(" ").last
-      low_pos = rule.split(" ").first.split('-').first.to_i
-      high_pos = rule.split(" ").first.split('-').last.to_i
+      low = rule.split(" ").first.split('-').first.to_i
+      high = rule.split(" ").first.split('-').last.to_i
       password = line.split(': ').last.chomp.split('')
 
-      positional_count = []
-      positional_count << (password[low_pos-1] == letter)
-      positional_count << (password[high_pos-1] == letter)
+      if p_one
+        letter_count = password.count(letter)
 
-      count += 1 if positional_count.count(true) == 1
+        count += 1 if letter_count >= low && letter_count <= high
+      else
+        positional_count = []
+        positional_count << (password[low-1] == letter)
+        positional_count << (password[high-1] == letter)
+  
+        count += 1 if positional_count.count(true) == 1
+      end  
     end
     count
   end
