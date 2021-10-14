21f026f63fe78e69f6b97025f48d761549800b97
sammo <samrhysperry93@hotmail.co.uk>
2021-10-14 16:00:57 +0100

    day 13 p1

---
+++ lib/13/shuttle_search.rb
@@ -0,0 +1,44 @@
+class ShuttleSearch
+  attr_reader :earliest_time, :buses, :times
+
+  def initialize(file)
+    data = File.open(file, 'r').readlines.map {|c| c.chomp }
+    @earliest_time = data.first.to_i
+    @buses = data.last.split(",").select {|bus| bus != "x"}.map(&:to_i)
+    @times = bus_times
+  end
+
+  def bus_times
+    times = {}
+    buses.each do |b|
+      t = [0]
+      100000.times do |n|
+        t << t[n] + b
+      end
+      times[b] = t
+    end
+    times
+  end
+
+  def solve_part_1
+    ebus = 0
+    waits = {}
+    times.each do |t|
+      a = (t.last << earliest_time).sort
+      idx = a.index(earliest_time)
+      if t[idx-1] == earliest_time || t[idx+1] == earliest_time
+        ebus = t.first
+      end
+
+      next_bus = a[idx+1]
+      waits[t.first] = (next_bus - earliest_time)
+    end
+
+    baw = waits.sort_by {|k, v| v}.first
+    bus = baw[0]
+    wait = baw[1]
+    bus * wait
+  end
+end
+
+p ShuttleSearch.new('./data.txt').solve_part_1
